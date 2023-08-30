159fe95358c123e165bca614efd02bd517b3129f
Jubayer Hossain <work.jubayer@gmail.com>
2023-08-30 21:24:29 +0600

    added themes

--- dengue2023.R
+++ dengue2023.R
@@ -36,6 +36,20 @@ ggplot(data, aes(x = Date))+
 
 
 
+fig3 <- ggplot(data = long_cases_data, 
+               aes(x = Months, y = Cases, group=Year, color=Year))+
+  geom_line()+
+  theme_minimal()+ 
+  theme(axis.text.x = element_text(angle = 60, vjust = 0.5, hjust=1))
+ggplotly(fig3)
 
+
+
+fig3 <- ggplot(data = long_cases_data, 
+               aes(x = Months, y = Cases, fill=Year))+
+  geom_bar(stat="identity")+
+  geom_line(aes(y = Cases))+ 
+  theme_minimal()
+ggplotly(fig3)
 
 
