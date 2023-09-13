9216fc90cc9f3feaf5a30113eb9455532b04e297
Jubayer Hossain <work.jubayer@gmail.com>
2023-09-13 15:05:39 +0600

    added % valuebox

--- scripts/map.R
+++ scripts/map.R
@@ -9,4 +9,15 @@ library(tidyverse)
 library(tmap)
 library(bangladesh)
 
+# all division data 
+division_map <- get_map("division")
+division_centroids <- bangladesh::get_coordinates(level = "division")
 
+
+# individual division 
+dhaka <- get_divisions(divisions = "Dhaka",level =  "upazila")
+# single division
+ggplot(data = dhaka) +
+  geom_sf() +
+  xlab("")+ ylab("")+
+  theme_minimal()
